c3ce7462faf94311d69f1cff50555324b4c533d0
mmosaad <johndoe@example.com>
2023-01-28 12:32:13 +0200

    fwwf

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import { Fragment, useState } from "react";
-import { nanoid } from "nanoid";
 import formInputs from "./lists/formInputs";
 import ErrorMessage from "./shared/ErrorMessage/ErrorMessage";
 import FormInput from "./shared/FormInput/FormInput";
-import FormSubmitBtn from "./shared/FormSubmitBtn/FormSubmitBtn";
-import { productDataValidation } from "./validation/productValidation";
+import FormSubmitBtn from "./shared/FormInput/FormSubmitBtn";
 
 /** ---------- COMPONENT ---------- */
 
 const App = () => {
   /** ---------- STATE ---------- */
-  const [productList, setProductList] = useState([]);
-  const [product, setProduct] = useState({
+  const [product, setUser] = useState({
     title: "",
     description: "",
     image: "",
@@ -19,59 +16,41 @@ const App = () => {
     brand: "",
     colors: [],
     // sizes: [], // TODO
-    // gender: "male"
   });
-  const [errors, setErrors] = useState({ ...product });
-  const [isError, setIsError] = useState(false);
 
   /** ---------- HANDLER ---------- */
-  const onChangeHandler = e => {
+  const onChangeHandler = (e) => {
     const {
       target: { name, value },
     } = e;
-    setProduct({ ...product, [name]: value });
-    setErrors({ ...errors, [name]: null });
-  };
 
-  const submitHandler = e => {
-    e.preventDefault();
-
-    setErrors(productDataValidation(product));
-
-    if (Object.keys(productDataValidation(product)).length) {
-      setIsError(true);
-      return;
-    }
-
-    setProductList([...productList, { ...product, id: nanoid() }]);
+    setUser({
+      ...product,
+      [name]: value,
+    });
   };
 
   /** ---------- RENDER ---------- */
-  const renderFormInputList = formInputs.map(({ label, name, type }, idx) => (
+  const renderFormInputList = formInputs.map((input, idx) => (
     <Fragment key={idx}>
       <FormInput
-        labelTxt={label}
-        id={name}
-        type={type}
-        name={name}
+        labelTxt={input.label}
+        id={input.name}
+        type={input.type}
+        name={input.name}
         value={product[name]}
         onChange={onChangeHandler}
       />
-
-      <ErrorMessage msg={errors[name]} />
+      <ErrorMessage />
     </Fragment>
   ));
 
   return (
     <div>
-      <form className="m-6" onSubmit={submitHandler}>
+      <form className="m-6">
         {renderFormInputList}
-        <FormSubmitBtn isError={isError} />
+        <FormSubmitBtn />
       </form>
-
-      {productList.map(({ id, title }) => (
-        <h2 key={id}>{title}</h2>
-      ))}
     </div>
   );
 };
